2fc4785448b961d11e930ed095f8fc18bc44d21c
Faisal Ahmed <faisal.ahmedbcse@gmail.com>
2019-10-01 21:15:46 +0600

    first commit

---
+++ App.js
@@ -0,0 +1,78 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Fragment } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import EventList from './EventList';
+
+import { ThemeProvider, Button } from 'react-native-elements';
+
+const App = () => {
+  const theme = {
+    Button: {
+      titleStyle: {
+        color: 'red',
+      },
+    },
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <Button title="My Button" />
+      <Button title="My 2nd Button" />
+    </ThemeProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
